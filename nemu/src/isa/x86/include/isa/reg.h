a00153c16736352cca7d17ea2db8639deb16087b
MrConnorKenway <auriaiur@outlook.com>
2020-09-18 19:02:22 +0800

    Re-organize the 'CPU_state' structure

--- nemu/src/isa/x86/include/isa/reg.h
+++ nemu/src/isa/x86/include/isa/reg.h
@@ -17,18 +17,25 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  */
 
 typedef struct {
-  struct {
-    uint32_t _32;
-    uint16_t _16;
-    uint8_t _8[2];
-  } gpr[8];
+  // anonymous union
+  union {
+    union {
+      uint32_t _32;
+      uint16_t _16;
+      uint8_t _8[2];
+    } gpr[8];
 
-  /* Do NOT change the order of the GPRs' definitions. */
+    /* Do NOT change the order of the GPRs' definitions. */
 
-  /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
-   * in PA2 able to directly access these registers.
-   */
-  rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+    /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
+     * in PA2 able to directly access these registers.
+     */
+
+    // anonymous struct
+    struct {
+      rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+    };
+  };
 
   vaddr_t pc;
 
@@ -50,10 +57,14 @@ static inline const char* reg_name(int index, int width) {
   assert(index >= 0 && index < 8);
 
   switch (width) {
-    case 4: return regsl[index];
-    case 1: return regsb[index];
-    case 2: return regsw[index];
-    default: assert(0);
+    case 4:
+      return regsl[index];
+    case 1:
+      return regsb[index];
+    case 2:
+      return regsw[index];
+    default:
+      assert(0);
   }
 }
 
