8def5b9279520a395fe4d50fbaa044d403f30289
MrConnorKenway <auriaiur@outlook.com>
2020-09-25 22:28:28 +0800

    Format cc.c

--- nemu/src/isa/x86/exec/cc.c
+++ nemu/src/isa/x86/exec/cc.c
@@ -2,12 +2,12 @@
 
 /* Condition Code */
 
-void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
+void rtl_setcc(rtlreg_t *dest, uint8_t subcode) {
   bool invert = subcode & 0x1;
   enum {
-    CC_O, CC_NO, CC_B,  CC_NB,
+    CC_O, CC_NO, CC_B, CC_NB,
     CC_E, CC_NE, CC_BE, CC_NBE,
-    CC_S, CC_NS, CC_P,  CC_NP,
+    CC_S, CC_NS, CC_P, CC_NP,
     CC_L, CC_NL, CC_LE, CC_NLE
   };
 
@@ -20,8 +20,7 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     case CC_BE:
     case CC_S:
     case CC_L:
-    case CC_LE:
-      TODO();
+    case CC_LE:TODO();
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
