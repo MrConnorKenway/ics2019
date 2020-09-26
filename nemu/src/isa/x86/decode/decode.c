92fa1d1f4893edf4a7e10e98598b0cdd281210c9
MrConnorKenway <auriaiur@outlook.com>
2020-09-26 11:07:53 +0800

    Sign extend imm8 when executing push(6A)

--- nemu/src/isa/x86/decode/decode.c
+++ nemu/src/isa/x86/decode/decode.c
@@ -37,7 +37,9 @@ static inline make_DopHelper(SI) {
     op->simm = (op->simm << 24) >> 24;
   }
 
-  rtl_li(&op->val, op->simm);
+  if (load_val) {
+    rtl_li(&op->val, op->simm);
+  }
 
   print_Dop(op->str, OP_STR_SIZE, "$0x%x", op->simm);
 }
