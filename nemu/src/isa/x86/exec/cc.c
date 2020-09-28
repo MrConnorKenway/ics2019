9e896adbd64887b0582bf0e03d1b76e76b391b7b
MrConnorKenway <auriaiur@outlook.com>
2020-09-28 20:04:35 +0800

    Implement jle(7E)

--- nemu/src/isa/x86/exec/cc.c
+++ nemu/src/isa/x86/exec/cc.c
@@ -24,7 +24,14 @@ void rtl_setcc(rtlreg_t *dest, uint8_t subcode) {
     case CC_BE:
     case CC_S:
     case CC_L:
-    case CC_LE:TODO();
+    case CC_LE: {
+      rtl_get_SF(&t0);
+      rtl_get_OF(&t1);
+      rtl_xor(&t1, &t0, &t1);
+      rtl_get_ZF(&t0);
+      rtl_or(dest, &t0, &t1);
+      break;
+    }
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
