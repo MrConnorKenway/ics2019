b649635ed408da59c5eb905a57366c57a6530077
MrConnorKenway <auriaiur@outlook.com>
2020-09-29 16:57:28 +0800

    Implement jmp(FF)

--- nemu/src/isa/x86/exec/all-instr.h
+++ nemu/src/isa/x86/exec/all-instr.h
@@ -40,9 +40,11 @@ make_EHelper(operand_size);
 make_EHelper(call);
 make_EHelper(call_rm);
 
-make_EHelper(ret);
 make_EHelper(jcc);
 make_EHelper(jmp);
+make_EHelper(jmp_rm);
+
+make_EHelper(ret);
 make_EHelper(leave);
 
 make_EHelper(nop);
