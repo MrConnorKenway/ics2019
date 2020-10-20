3635ef2ed33c594fea99800dfe6c1e2caf9894d7
MrConnorKenway <auriaiur@outlook.com>
2020-10-20 14:50:06 +0800

    Add handler for syscall event and implement sys_yield

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,7 +1,14 @@
 #include "common.h"
+#include "syscall.h"
+
+_Context* do_syscall(_Context *);
 
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
+    case _EVENT_SYSCALL: {
+      do_syscall(c);
+      break;
+    }
     case _EVENT_YIELD: printf("_EVENT_YIELD\n"); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
