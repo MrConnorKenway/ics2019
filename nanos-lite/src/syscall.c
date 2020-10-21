6f6bfbddcc76e21afedc0fe788afa1aede5a1749
MrConnorKenway <auriaiur@outlook.com>
2020-10-21 11:29:55 +0800

    Implement sys_write for stdout and stderr

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -11,6 +11,20 @@ _Context* do_syscall(_Context *c) {
   switch (a[0]) {
     case SYS_exit: _halt(a[1]);
     case SYS_yield: _yield(); c->GPRx = 0; break;
+    case SYS_write: {
+      int fd = a[1];
+      size_t count = a[3];
+      if (fd == 1 || fd == 2) {
+        uint8_t *buf = (uint8_t *) a[2];
+        for (size_t i = 0; i < count; ++i) {
+          _putc(((uint8_t *) buf)[i]);
+        }
+        c->GPRx = count;
+      } else {
+        panic("Unsupported fd %d", fd);
+      }
+      break;
+    }
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
